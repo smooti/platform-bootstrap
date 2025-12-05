90541b1a7117d2eec7af13d2e5a70ccab093e88c
smooti1 <kobeowens43099@gmail.com>
2025-12-05 13:41:54 -0500

    Adding utilities script containing reused modules

---
+++ scripts/utilities.sh
@@ -0,0 +1,12 @@
+set -e
+
+# Colors
+export RED='\033[38;5;160m'
+export GREEN='\033[38;5;34m'
+export PURPLE='\033[38;5;98m'
+export NC='\033[0m'
+export BLUE='\033[38;5;33m'
+export YELLOW='\033[38;5;178m'
+export CYAN='\033[38;5;37m'
+export ORANGE='\033[38;5;172m'
+export BOLD='\033[1m'
